4b627c1320a1d16b1cd67c687a60522c3ff2f716
swaroopanand <swaroopanand695@gmail.com>
2022-04-24 16:21:54 +0530

    updated project with a lot of things

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+
+app.get("/", function(req , res){
+
+})
+app.listen(5000, function(){
+  console.log("server is running");
+});
+
+
--- src/components/library.jsx
+++ src/components/library.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import {FormGroup} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FloatingLabel, Form, Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import {} from 'react-bootstrap';
 function Library() {
   return(
     <div>
