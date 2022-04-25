4a021c2ede923e105aedae51365642eaa61e015d
swaroopanand <swaroopanand695@gmail.com>
2022-04-25 22:39:31 +0530

    updated project with a lot of things

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const User = new Schema({
+const UserSchema = new Schema({
   fname: { type: String, required:true },
   email: { type: String, required:true, unique:true },
   password: { type: String, required:true },
 }
 ) 
-module.exports = mongoose.model('UserData', User)
-
+const User = mongoose.model('UserData', UserSchema)
+User.createIndexes();
+module.exports = User
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require('cors')
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const User = require('../models/user.js')
 // const app = express();
 // app.use(cors())
@@ -13,16 +14,33 @@ router.get('/', (req, res) => {
 })
 
 
-router.post("/", async (req,res)=>{
+router.post("/", [
+    body('fname', 'Enter a valid name').isLength({ min: 2 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Enter a valid password').isLength({ min: 3 }),
+  ],
+   async (req,res)=>{
   console.log(req.body)
-  try{
-  const user = User(req.body); 
-  await user.save()
-  res.json({ status: 'ok'})
-  }
-	 catch (err) {
+    try{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    await User.create({
+        fname: req.body.fname,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json({ status:'ok'}));
+    }
+   catch (err) {
 		res.json({ status: 'error', error: 'Duplicate email' })
 	}
+  })
+  // try{
+  // const user = User(req.body); 
+  // await user.save()
+  // res.json({ status: 'ok'})
+  // }
  //  try {
 	// 	await User.create({
 	// 		fname: req.body.fname,
@@ -33,6 +51,6 @@ router.post("/", async (req,res)=>{
 	// } catch (err) {
 	// 	res.json({ status: 'error', error: 'Duplicate email' })
 	// }
-})
+
 
 module.exports=router
